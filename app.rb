fef0af0a093cb402f0fa8f1f3c22855f82ab47a2
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-01-18 09:18:08 -0500

    Add rook class

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 # app.rb
 
 require_relative("lib/king.rb")
+require_relative("lib/rook.rb")
+
+# --------------------------
+# --------- King -----------
+# --------------------------
 
 black_king = King.new(5, 8, "black")
 
@@ -15,5 +20,25 @@ puts "Bad moves"
 p white_king.move?(7, 2)
 p white_king.move?(1, 5)
 p white_king.move?(1, 1)
+
+puts "\n------------------\n"
+
+
+# --------------------------
+# --------- Rook -----------
+# --------------------------
+
+black_rook_left = Rook.new(1, 8, "black")
+black_rook_right = Rook.new(8, 8, "black")
+white_rook_left = Rook.new(1, 1, "white")
+white_rook_right = Rook.new(8, 1, "white")
+
+puts "\nTesting the Rook"
+puts "Good moves"
+p white_rook_right.move?(8, 5)
+p white_rook_right.move?(2, 1)
+puts "Bad moves"
+p white_rook_right.move?(6, 3)
+p white_rook_right.move?(2, 3)
 
 puts "\n------------------\n"
